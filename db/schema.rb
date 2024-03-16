807714d6fa026b5f258b7d38fb5d7d5e4d0e5db9
Halima Ablat <hailiman1029@hotmail.com>
2024-03-17 00:12:49 +1100

    completed the task

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_15_234935) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_16_025357) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
